3128ecc5c024b4b47bd0de21b91c4d2523c9ef32
Grant <ghwilley@gmail.com>
2016-09-22 14:27:13 -0500

    started basic prog, updating

--- Before001/Program.cs
+++ Before001/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Before001
+﻿using System;
+using System.Collecitons.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Before001
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            int a,b,d;
+            a = 1;
+            b = 2;
+            d = 5;
+            if (a > b)
+            {
+                Console.WriteLine("AYY")
+            }
         }
     }
 }
